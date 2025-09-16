516aa99b6d662eb659b12bab2d9acd5a3194e99a
nuux <nuuxdev@gmail.com>
2025-09-16 17:10:52 +0300

    BUILD-FIX| new favicon fixed apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 export default function Home() {
   return (
     <div className="container">
-      <h1>NuuX's Personal Site</h1>
+      <h1>NuuX&apos;s Personal Site</h1>
       <p style={{ textAlign: "center", maxWidth: "40rem" }}>
-        This is my personal site. Yes, it's empty,😆 BUT it's a work in
-        progress. <br /> U know what makes it different from my other portfolio
-        sites? <br /> It's not empty. <br />
-        It's out there. <br />
+        This is my personal site. Yes, it&apos;s empty,😆 BUT it&apos;s a work
+        in progress. <br /> U know what makes it different from my other
+        portfolio sites? <br /> It&apos;s not empty. <br />
+        It&apos;s out there. <br />
         And it is Goddamn resposive 😎
         <br />
-        And that's the point.
+        And that&apos;s the point.
       </p>
       <h3>
         NB: It will be different next time you visit. Promise. Await. But not
