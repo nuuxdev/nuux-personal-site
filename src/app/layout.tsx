e217afdf0dd05cbc74e80be6b26dbe0094023745
nuux <nuuxdev@gmail.com>
2025-09-17 11:42:43 +0300

    FIX| darkmode flicker resolved damn... cookies are good

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import { cookies } from "next/headers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,13 +18,16 @@ export const metadata: Metadata = {
   description: "the personal site of NuuX",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const theme = cookieStore.get("nuux-user-mode")?.value ?? null;
+
   return (
-    <html lang="en">
+    <html lang="en" data-theme={theme}>
       <body
         className={`${martianMono.variable} ${geistSans.variable} antialiased`}
       >
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,30 +4,39 @@ import { useEffect, useState } from "react";
 export default function Header() {
   const [darkMode, setDarkMode] = useState<boolean>();
   useEffect(() => {
+    let resolvedTheme: "dark" | "light";
+
     if (darkMode === undefined) {
-      const savedMode = localStorage.getItem("nuux-user-mode");
-      if (savedMode === null) {
-        const prefersDarkMode = window.matchMedia(
-          "(prefers-color-scheme: dark)"
-        ).matches;
-        setDarkMode(prefersDarkMode);
+      const cookieValue = document.cookie
+        .split(";")
+        .map((c) => c.trimStart())
+        .find((c) => c.startsWith("nuux-user-mode="))
+        ?.split("=")[1];
+      if (cookieValue) {
+        resolvedTheme = cookieValue as "dark" | "light";
       } else {
-        setDarkMode(savedMode === "dark" ? true : false);
+        resolvedTheme = window.matchMedia("(prefers-color-scheme: dark)")
+          .matches
+          ? "dark"
+          : "light";
       }
-      return;
+      setDarkMode(resolvedTheme === "dark");
+    } else {
+      resolvedTheme = darkMode ? "dark" : "light";
     }
-    const html = document.documentElement;
-    html.setAttribute("data-theme", darkMode ? "dark" : "light");
-    localStorage.setItem("nuux-user-mode", darkMode ? "dark" : "light");
+
+    document.cookie = `nuux-user-mode=${resolvedTheme}; path=/; max-age=31536000`;
+    document.documentElement.setAttribute("data-theme", resolvedTheme);
   }, [darkMode]);
+
   return (
     <header>
-     <div className="nav_container">
-      <h3>NuuX</h3>
-      <button onClick={() => setDarkMode(!darkMode)}>
-        light-dark&#40;&#41;
-      </button>
-     </div>
+      <div className="nav_container">
+        <h3>NuuX</h3>
+        <button onClick={() => setDarkMode(!darkMode)}>
+          light-dark&#40;&#41;
+        </button>
+      </div>
     </header>
   );
 }
