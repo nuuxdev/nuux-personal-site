f432676dd0ed32ce6f5b9f2ff26f52fb28f8dda7
nuux <nuuxdev@gmail.com>
2025-09-18 17:53:50 +0300

    DAMN| wass crying till i make this work

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,37 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Header() {
   const [darkMode, setDarkMode] = useState<boolean>();
+  const modeChangeTlRef = useRef<gsap.core.Timeline | null>(null);
+  const hoverTlRef = useRef<gsap.core.Timeline | null>(null);
+  useGSAP(() => {
+    modeChangeTlRef.current = gsap
+      .timeline({ paused: true })
+      .to("#celestial_rays", {
+        fill: "var(--background)",
+        strokeWidth: 0,
+        strokeDasharray: "40px 0px",
+        cx: 26,
+        cy: 12,
+      })
+      .to(
+        "#celestial_base",
+        {
+          r: 14,
+        },
+        "<"
+      );
+    hoverTlRef.current = gsap
+      .timeline({ paused: true })
+      .to(
+        "#celestial_rays",
+        { "--radius": 12, duration: 0.3, ease: "power1.out" },
+        0
+      );
+  });
   useEffect(() => {
     let resolvedTheme: "dark" | "light";
 
@@ -13,31 +42,25 @@ export default function Header() {
         .find((c) => c.startsWith("nuux-user-mode="))
         ?.split("=")[1];
       if (cookieValue) {
+        modeChangeTlRef.current?.reversed(cookieValue !== "dark");
+        modeChangeTlRef.current?.progress(cookieValue === "dark" ? 1 : 0);
         resolvedTheme = cookieValue as "dark" | "light";
       } else {
         resolvedTheme = window.matchMedia("(prefers-color-scheme: dark)")
           .matches
           ? "dark"
           : "light";
+        modeChangeTlRef.current?.reversed(resolvedTheme !== "dark");
+        modeChangeTlRef.current?.progress(resolvedTheme === "dark" ? 1 : 0);
       }
       setDarkMode(resolvedTheme === "dark");
     } else {
+      modeChangeTlRef.current?.reversed(!darkMode);
       resolvedTheme = darkMode ? "dark" : "light";
     }
-
     document.cookie = `nuux-user-mode=${resolvedTheme}; path=/; max-age=31536000`;
     document.documentElement.setAttribute("data-theme", resolvedTheme);
   }, [darkMode]);
-
-  const handleModeChange = () => {
-    setDarkMode(!darkMode);
-    // const celestial_rays = document.getElementById("celestial_rays");
-    // const celestial_base = document.getElementById("celestial_base");
-    // if (celestial_rays && celestial_base) {
-    //   celestial_rays.setAttribute("r", "12");
-    //   celestial_base.setAttribute("r", "8");
-    // }
-  };
 
   return (
     <header>
@@ -48,7 +71,17 @@ export default function Header() {
             <img src="/nuux-logo.svg" alt="X" />
           </span>
         </h3>
-        <button className="icon_button" onClick={handleModeChange}>
+        <button
+          className="icon_button"
+          onClick={() => {
+            console.log(modeChangeTlRef.current?.reversed());
+            console.log(modeChangeTlRef.current?.progress());
+            modeChangeTlRef.current?.reverse();
+            setDarkMode(!darkMode);
+          }}
+          onMouseEnter={() => hoverTlRef.current?.play()}
+          onMouseLeave={() => hoverTlRef.current?.reverse()}
+        >
           <svg width="36" height="36" viewBox="0 0 36 36">
             <circle id="celestial_base" cx="18" cy="18" />
             <circle id="celestial_rays" cx="18" cy="18" />
