a4b02aedc3b001dda49553d705eb2abd0a1d8626
nuux <nuuxdev@gmail.com>
2025-09-18 22:46:46 +0300

    FIX|CHANGE| idk may be css is better than gsap for lightweight animations... Maybe

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,40 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Header() {
   const [darkMode, setDarkMode] = useState<boolean>();
+  const modeChangeRef = useRef<HTMLButtonElement>(null);
   useEffect(() => {
     let resolvedTheme: "dark" | "light";
-
-    if (darkMode === undefined) {
-      const cookieValue = document.cookie
-        .split(";")
-        .map((c) => c.trimStart())
-        .find((c) => c.startsWith("nuux-user-mode="))
-        ?.split("=")[1];
-      if (cookieValue) {
-        resolvedTheme = cookieValue as "dark" | "light";
-      } else {
-        resolvedTheme = window.matchMedia("(prefers-color-scheme: dark)")
-          .matches
-          ? "dark"
-          : "light";
-      }
-      setDarkMode(resolvedTheme === "dark");
+    const cookieValue = document.cookie
+      .split(";")
+      .map((c) => c.trimStart())
+      .find((c) => c.startsWith("nuux-user-mode="))
+      ?.split("=")[1];
+    if (cookieValue) {
+      resolvedTheme = cookieValue as "dark" | "light";
     } else {
-      resolvedTheme = darkMode ? "dark" : "light";
+      resolvedTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
     }
-
+    setDarkMode(resolvedTheme === "dark");
     document.cookie = `nuux-user-mode=${resolvedTheme}; path=/; max-age=31536000`;
     document.documentElement.setAttribute("data-theme", resolvedTheme);
-  }, [darkMode]);
+
+    if (resolvedTheme === "dark")
+      modeChangeRef.current?.classList.add("dark_mode");
+  }, []);
 
   const handleModeChange = () => {
+    const newTheme = darkMode ? "light" : "dark";
+    document.cookie = `nuux-user-mode=${newTheme}; path=/; max-age=31536000`;
+    document.documentElement.setAttribute("data-theme", newTheme);
     setDarkMode(!darkMode);
-    // const celestial_rays = document.getElementById("celestial_rays");
-    // const celestial_base = document.getElementById("celestial_base");
-    // if (celestial_rays && celestial_base) {
-    //   celestial_rays.setAttribute("r", "12");
-    //   celestial_base.setAttribute("r", "8");
-    // }
+    if (modeChangeRef !== null) {
+      if (darkMode) modeChangeRef.current?.classList.remove("dark_mode");
+      else modeChangeRef.current?.classList.add("dark_mode");
+    }
   };
 
   return (
@@ -48,7 +46,11 @@ export default function Header() {
             <img src="/nuux-logo.svg" alt="X" />
           </span>
         </h3>
-        <button className="icon_button" onClick={handleModeChange}>
+        <button
+          ref={modeChangeRef}
+          className="icon_button"
+          onClick={handleModeChange}
+        >
           <svg width="36" height="36" viewBox="0 0 36 36">
             <circle id="celestial_base" cx="18" cy="18" />
             <circle id="celestial_rays" cx="18" cy="18" />
