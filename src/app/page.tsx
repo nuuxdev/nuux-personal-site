a68c8fe1e62f77895e08d764256184a3757059c8
nuux <nuuxdev@gmail.com>
2025-09-19 13:56:52 +0300

    FIX| content-grid kinda great for setting all the contents size in a single context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,56 @@
 import Header from "@/components/header";
-import ProjectsButton from "@/components/projectsButton";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className="container">
-        <h1>
-          I am <span id="custom_emoji">NuuX🕶️</span>
-        </h1>
-        <p style={{ textAlign: "center", maxWidth: "40rem" }}>
-          This is my personal site. Yes, there&apos;s not much to see,😆 BUT
-          it&apos;s a work in progress. <br /> U know what makes it different
-          from my other portfolio projects? <br /> It&apos;s not empty. <br />
-          It&apos;s out there... damn right <br />
-          And it is Goddamn responsive 😎
-          <br />
-          And that&apos;s the point.
-        </p>
-        <h3>
-          <span>NB</span>: It will be different next time you visit. Promise.
-          Await. But not All 😁
-        </h3>
-        <ProjectsButton />
-      </section>
-      <section id="projects" style={{ height: "200vh" }}>
-        <h1>Projects</h1>
-        <p style={{ textAlign: "center" }}>
-          My Amazing Projects will be listed here
-        </p>
-        <h3>
-          <span>Soon</span>😁
-        </h3>
-      </section>
+      <main className="content_grid">
+        <section className="hero_section breakout">
+          <h1>
+            I am <span id="custom_emoji">NuuX🕶️</span>
+          </h1>
+          <p style={{ textAlign: "center" }}>
+            This is my personal site. Yes, there&apos;s not much to see,😆 BUT
+            it&apos;s a work in progress. U know what makes it different from my
+            other portfolio projects? It&apos;s not empty. It&apos;s out
+            there... damn right And it is Goddamn responsive 😎 And that&apos;s
+            the point.
+          </p>
+          <h3>
+            <span>NB</span>: It will be different next time you visit. Promise.
+            Await. But not All 😁
+          </h3>
+          <Link href="#projects" style={{ alignSelf: "center" }}>
+            <button tabIndex={-1}>
+              Straight to my projects{" "}
+              <span className="button_icon_emoji">⛷️</span>
+            </button>
+          </Link>
+        </section>
+        <section
+          id="projects"
+          className="fullscreen"
+          style={{ height: "100vh" }}
+        >
+          <h1>Projects</h1>
+          <p style={{ textAlign: "center" }}>
+            My Amazing Projects will be listed here
+          </p>
+          <h3>
+            <span>Soon</span>😁
+          </h3>
+        </section>
+        <section id="another" style={{ height: "100vh" }}>
+          <h1>Another</h1>
+          <p style={{ textAlign: "center" }}>
+            My Amazing another things will be listed here
+          </p>
+          <h3>
+            <span>Soon</span>😁
+          </h3>
+        </section>
+      </main>
     </>
   );
 }
