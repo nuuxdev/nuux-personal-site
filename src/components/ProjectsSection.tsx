58115ed2f7e10e98fb81ad129a498c73652c8cfc
nuux <nuuxdev@gmail.com>
2025-11-07 16:21:52 +0300

    FIX| mobiile projects added

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,35 +1,49 @@
+import Link from "next/link";
+import { FigmaIcon } from "./ToolIcons";
+
 export default function ProjectsSection() {
   return (
     <section id="projects" className="fullscreen">
       <h1>Projects</h1>
-      <p style={{ textAlign: "center" }}>
-        My Amazing Projects will be listed here
-      </p>
+      {/* Mobile */}
       <h3>
-        <span>Mobile</span>😁
+        <span>Mobile Designs</span>
       </h3>
+      <p style={{ textAlign: "center" }}>
+        <span><FigmaIcon />igma</span> powered responsive designs with creativity 🪄 and interactivity 💯
+      </p>
+      
       <div className="mobile_projects_container">
         <figure className="mobile_projects">
-          <img src="/projects/bg-mobile-light.webp" alt="project name" />
-          <figcaption>Project One oveflowing</figcaption>
+                    <Link target="blank" href="https://www.figma.com/proto/fSVsJ4bl3cZqLMtfOG91Fc/Nova?t=3QU6AZidDnbUKNbI-1&scaling=scale-down&content-scaling=responsive&page-id=0%3A1&node-id=6-964&starting-point-node-id=6%3A964&show-proto-sidebar=1">
+<img src="/projects/nova-erp-one.webp" alt="Nova ERP" /></Link>
+          <figcaption>ERP</figcaption>
         </figure>
         <figure className="mobile_projects">
-          <img src="/projects/bg-mobile-light.webp" alt="project name" />
-          <figcaption>Project Two</figcaption>
+                    <Link target="blank" href="https://www.figma.com/proto/iEyNQK94pVTOPIPGRRQ67x/Nova-Tracker?t=sPw1Dgs5bXHGY6G4-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&node-id=22-885&starting-point-node-id=22%3A885&show-proto-sidebar=1">
+<img src="/projects/nova-expense-one.webp" alt="Nova Expense Tracker" /></Link>
+          <figcaption>Expense Tracker</figcaption>
         </figure>
         <figure className="mobile_projects">
-          <img src="/projects/bg-mobile-light.webp" alt="project name" />
-          <figcaption>Project Three</figcaption>
+                    <Link target="blank" href="https://www.figma.com/proto/C0m5JRoGn8FDeWPIexZZ5w/beete-Nibab?t=9jZQpRQxKxl8jz2T-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&node-id=1095-802&starting-point-node-id=1095%3A685&show-proto-sidebar=1">
+<img src="/projects/beete-nibab.webp" alt="Bete Nibab" /></Link>
+          <figcaption>Bete Nibab</figcaption>
         </figure>
         <figure className="mobile_projects">
-          <img src="/projects/bg-mobile-light.webp" alt="project name" />
-          <figcaption>Project Four</figcaption>
+                   <Link target="blank" href="https://www.figma.com/proto/sSLu1trcyb1HtU76R2AMQm/Crib-CRM-redesign?node-id=46-834&p=f&t=IxKVw6M5M9SmMr1a-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=13%3A205&show-proto-sidebar=1">
+ <img src="/projects/crib-crm.webp" alt="Crib CRM" />
+ </Link>
+          <figcaption>Crib CRM</figcaption>
         </figure>
         <figure className="mobile_projects">
-          <img src="/projects/bg-mobile-light.webp" alt="project name" />
-          <figcaption>Project Five</figcaption>
+          <Link target="blank" href="https://www.figma.com/proto/5PUlzUqWYMLbh1YJkASu6e/ERF-sabadmin?node-id=1109-1071&p=f&viewport=591%2C378%2C0.11&t=pqdyFUn9ZioENPuo-1&scaling=scale-down&content-scaling=fixed&page-id=1105%3A2&starting-point-node-id=1105%3A4">
+          <img src="/projects/erf-rooms.webp" alt="Erf Rooms" />
+          </Link>
+          <figcaption>Erf Rooms</figcaption>
         </figure>
       </div>
+      {/* Web */}
+      
     </section>
   );
 }
